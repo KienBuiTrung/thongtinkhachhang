a515555c7c9d822e0c67471d7255d6b5082a5dca
Kien Bui <kienbt4@bidv.com.vn>
2017-09-30 17:06:04 +0700

    Seed FROM CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,3 +9,14 @@
 
 admin = User.create(name: "Bùi Trung Kiên", email: "kienbt4@bidv.com.vn", department: "QLNB", password: "12345678", 
 	password_confirmation: "12345678", is_admin: true)
+
+
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'users.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  User.create(row.to_hash)
+  puts "saved"
+end
+
+puts "Done Seeding!!!"
